ba977432f0f6c78f0d72517adb5818fb276c8502
Mathieu M <m.mercier78@gmail.com>
2021-09-09 20:33:09 +0200

    Mise en place intersectionObserver pour les animations au scroll

--- index.js
+++ index.js
@@ -35,9 +35,56 @@ $(window).scroll(function() {
 
     // Assign active class to nav links while scrolling
     $('section').each(function(i) {
-            if ($(this).position().top -100 <= scrollDistance) { // -120 pour le décalage dû au header
-                    $('nav a.active').removeClass('active');
-                    $('nav a').eq(i).addClass('active');
+            if ($(this).position().top -120 <= scrollDistance) { // -120 pour le décalage dû au header
+                    $('nav ul a.active').removeClass('active');
+                    $('nav ul a').eq(i).addClass('active');
             }
     });
 }).scroll();
+
+
+// IntersectionObserver pour apparition vers le haut au scroll (ajout classe appear)
+
+const cartes = document.querySelectorAll(".carte-service", { passive: true });
+
+const options = {
+    root: null, // it is the viewport
+    threshold: 0,
+    rootMargin: "-150px"
+};
+
+const observer = new IntersectionObserver(function
+(entries, observer) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        }
+        entry.target.classList.add("appear");
+        observer.unobserve(entry.target);
+    });
+}, options);
+
+cartes.forEach(carte => {
+    observer.observe(carte);
+})
+
+// IntersectionObserver pour apparition vers au scroll (ajout classe appear2 sur les eléments avec une class anim)
+
+const animations = document.querySelectorAll(".anim", { passive: true });
+
+const observer2 = new IntersectionObserver(function
+(entries, observer) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        }
+    entry.target.classList.add("appear2");
+    observer.unobserve(entry.target);
+    });
+}, options);
+
+
+animations.forEach(elt => {
+    observer2.observe(elt);
+})
+
